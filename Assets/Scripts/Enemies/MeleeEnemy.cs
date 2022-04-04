c69ff140bea9ce8db86af91a1ee5d4e666384768
Paolo Tormon <paolotormon@gmail.com>
2022-04-04 21:06:22 +0800

    Add Knight MeleeAttack Logic

--- Assets/Scripts/Enemies/MeleeEnemy.cs
+++ Assets/Scripts/Enemies/MeleeEnemy.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class MeleeEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private int damage;
+    [SerializeField] private BoxCollider2D boxCollider2D;
+    [SerializeField] private LayerMask playerLayer;
+
+    private float cooldownTimer = Mathf.Infinity;
+
+    //Refs
+    private Animator anim;
+    private Health playerHealth;
+
+
+    private void Awake()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        cooldownTimer += Time.deltaTime;
+        //Attack only when player in sight
+        if (PlayerInSight())
+        {
+            if (cooldownTimer >= attackCooldown)
+            {
+                cooldownTimer = 0;
+                anim.SetTrigger("meleeAttack");
+            }
+        }
+
+    }
+    private bool PlayerInSight()
+    {
+        //args
+        //1st = origin (flip depending on localscalex)
+        //2nd = change size of box
+        Vector3 origin = boxCollider2D.bounds.center + transform.right * range * transform.localScale.x * colliderDistance;
+        Vector3 size = new Vector3(boxCollider2D.bounds.size.x * range, boxCollider2D.bounds.size.y, boxCollider2D.bounds.size.z);
+
+        RaycastHit2D hit = Physics2D.BoxCast(origin, size, 0, Vector2.left, 0, playerLayer);
+
+        if (hit.collider != null)
+            playerHealth = hit.transform.GetComponent<Health>();
+
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Vector3 origin = boxCollider2D.bounds.center + transform.right * range * transform.localScale.x * colliderDistance;
+        Vector3 size = new Vector3(boxCollider2D.bounds.size.x * range, boxCollider2D.bounds.size.y, boxCollider2D.bounds.size.z);
+
+        Gizmos.DrawWireCube(origin, size);
+    }
+
+    private void DamagePlayer()
+    {
+        if (PlayerInSight())
+        {
+            playerHealth.TakeDamage(damage);
+        }
     }
 }
